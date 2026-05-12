da77b8a5d150275e87d0f132ccc4374fd610bf39
Wycliffe Yegon <captainwycliffe@gmail.com>
2026-05-13 02:40:05 +0300

    feat(app): point mobile app to production Render backend

--- MaragoliApp/constants/api.js
+++ MaragoliApp/constants/api.js
@@ -1,3 +1,1 @@
-// Change this to your hosted backend URL once deployed
-// e.g. 'https://maragoli-backend.onrender.com'
-export const API_BASE_URL = 'http://192.168.0.102:3000';
+export const API_BASE_URL = 'https://maragoliapp.onrender.com';
